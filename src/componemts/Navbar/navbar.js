daa0abd7ccf6d98701082ba3f3fce388b934cfb0
Stupood <akwitiya@gmail.com>
2025-04-01 14:29:21 +0700

    Update project

--- src/componemts/Navbar/navbar.js
+++ src/componemts/Navbar/navbar.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router'
 import './navbar.css'
-import { useDataContext } from '../context/context'
 
 export default function Navbar(){
-  const {dispatch} = useDataContext()
   return (
     <nav className='navbar'>
     <div className='nav container'>
@@ -14,7 +12,6 @@ export default function Navbar(){
           <Link to="/" >Home</Link>
         </li>
         <li>
-          {/* <Link to="/report" onClick={()=>dispatch({type:"NAVIGATE"})}>Report</Link> */}
           <Link to="/report">Report</Link>
         </li>
       </ul>
